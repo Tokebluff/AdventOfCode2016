77cfc2dd33052bfbdd809d6905ba543e7f9b2e1b
Paul Dimitrie Pistea <ppdimi@gmail.com>
2016-12-06 05:13:41 +0000

    day6

---
+++ Advent of code 2016/Advent of code 2016/Day6.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Advent_of_code_2016
+{
+    public static class Day6
+    {
+        public static void Run()
+        {
+            var inputs = File.ReadAllLines(Environment.CurrentDirectory + "/inputs/inputDay6.txt");
+            char[] cPart1 = new char[8];
+            char[] cPart2 = new char[8];
+
+            for (var i = 0; i < 8; i++)
+            {
+                var list = new List<char>();
+                for(var j = 0; j < inputs.Length; j++)
+                {
+                    list.Add(inputs[j][i]);
+                }
+                var k = list.GroupBy(x => x).Select(g => new { Text = g.Key, Count = g.Count() });
+
+                cPart1[i] = k.OrderByDescending(x => x.Count).First().Text;
+                cPart2[i] = k.OrderBy(x => x.Count).First().Text;
+            }
+            Console.WriteLine("Part 1: " + new string(cPart1));
+            Console.WriteLine("Part 2: " + new string(cPart2));
+        }
+    }
+}
--- Advent of code 2016/Advent of code 2016/Program.cs
+++ Advent of code 2016/Advent of code 2016/Program.cs
@@ -10,7 +10,7 @@ namespace Advent_of_code_2016
     {
         static void Main(string[] args)
         {
-            Day5.Run();
+            Day6.Run();
             Console.ReadLine();
 
         }
